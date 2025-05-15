9ed54c47ecafa88b22b7baa6939bf8cd87e05b0f
Piyush Singh <b221037@iiit-bh.ac.in>
2025-05-15 12:54:04 +0530

    Version changes

--- obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+215583c0cc43d88e86c2c812b65b77a7c4e2e76e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c98b1e03079d64654b197ee37f92d752c797e163")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
